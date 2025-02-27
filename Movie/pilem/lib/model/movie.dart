f8191d20897323afeb33126e070bbed313394d18
Jennifer Verty 2327250040 <jenniferverty0605@gmail.com>
2025-02-27 11:13:19 +0700

    Jenni_2327250040

---
+++ Movie/pilem/lib/model/movie.dart
@@ -0,0 +1,31 @@
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterpath;
+  final String backdropPath;
+  final String releaseDate;  
+  final String voteAverage;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterpath,
+    required this.backdropPath,
+    required this.releaseDate,  
+    required this.voteAverage,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterpath: json['posterpath'],
+      backdropPath: json['backdropPath'],  
+      releaseDate: json['releaseDate'],    
+      voteAverage: json['voteAverage'],
+    );
+  }
+}
---
+++ Movie/pilem/lib/services/api_services.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiServices {
+  static const String apikey = 'c6ec2d98d4844f76a9a5f4ed9854dbbd';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+
+  Future<List<Map<String, dynamic>>> getAllMovie() async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/movie/now_playing?api_key=$apikey'),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovie() async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/trending/movie/week?api_key=$apikey'),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/movie/popular?api_key=$apikey'),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
